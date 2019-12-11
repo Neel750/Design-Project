c05b2ebb22597eef700a5395b920dd4d2add7c9c
Neel750 <170010116050@adit.ac.in>
2019-12-11 21:44:42 +0530

    Success

--- Backend/PossiblePlate.py
+++ Backend/PossiblePlate.py
@@ -3,10 +3,8 @@
 import cv2
 import numpy as np
 
-###################################################################################################
 class PossiblePlate:
 
-    # constructor #################################################################################
     def __init__(self):
         self.imgPlate = None
         self.imgGrayscale = None
@@ -15,10 +13,6 @@ class PossiblePlate:
         self.rrLocationOfPlateInScene = None
 
         self.strChars = ""
-    # end constructor
-
-# end class
-
 
 
 
